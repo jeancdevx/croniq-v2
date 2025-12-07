35755af8ed4a1cc42fe14ffc0c3f1a3501b24e52
jeancdev <jcode2006@gmail.com>
2025-12-07 11:16:39 -0500

    feat(dashboard): simplify layout and enhance breadcrumb navigation

--- app/(dashboard)/(routes)/layout.tsx
+++ app/(dashboard)/(routes)/layout.tsx
@@ -1,60 +1,21 @@
-'use client'
-
+import { DashboardBreadcrumb } from '@/modules/dashboard/ui/components/dashboard-breadcrumb'
 import { DashboardSidebar } from '@/modules/dashboard/ui/components/dashboard-sidebar'
 
-import {
-  Breadcrumb,
-  BreadcrumbItem,
-  BreadcrumbLink,
-  BreadcrumbList,
-  BreadcrumbPage,
-  BreadcrumbSeparator
-} from '@/components/ui/breadcrumb'
-import { Separator } from '@/components/ui/separator'
-import {
-  SidebarInset,
-  SidebarProvider,
-  SidebarTrigger
-} from '@/components/ui/sidebar'
-
-import { useBreadcrumbs } from './hooks/use-breadcrumbs'
+import { SidebarInset, SidebarProvider } from '@/components/ui/sidebar'
 
 interface DashboardLayoutProps {
   children: React.ReactNode
 }
 
 export default function DashboardLayout({ children }: DashboardLayoutProps) {
-  const breadcrumbs = useBreadcrumbs()
-
   return (
     <SidebarProvider>
       <DashboardSidebar />
 
       <SidebarInset className='m-0!'>
-        <header className='sticky top-0 flex h-16 shrink-0 items-center gap-2 transition-[width,height] ease-linear group-has-data-[collapsible=icon]/sidebar-wrapper:h-12'>
-          <div className='flex items-center gap-2 px-4'>
-            <SidebarTrigger className='-ml-1' />
-            <Separator
-              orientation='vertical'
-              className='mr-2 data-[orientation=vertical]:h-4'
-            />
-            <Breadcrumb>
-              <BreadcrumbList>
-                <BreadcrumbItem className='hidden md:block'>
-                  <BreadcrumbLink href='#'>
-                    {breadcrumbs.category}
-                  </BreadcrumbLink>
-                </BreadcrumbItem>
-                <BreadcrumbSeparator className='hidden md:block' />
-                <BreadcrumbItem>
-                  <BreadcrumbPage>{breadcrumbs.page}</BreadcrumbPage>
-                </BreadcrumbItem>
-              </BreadcrumbList>
-            </Breadcrumb>
-          </div>
-        </header>
+        <DashboardBreadcrumb />
 
-        <div className='min-h-[calc(100vh-4rem)] w-full'>{children}</div>
+        <div className='min-h-[calc(100vh-4rem)] px-4'>{children}</div>
       </SidebarInset>
     </SidebarProvider>
   )
