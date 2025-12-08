b5178d95436d1810b981c8e2093be853db177f05
Eddu <azabacheeduardo85@gmail.com>
2025-12-08 15:42:03 -0500

    feat: add pago flow relation

--- db/schema/relations.ts
+++ db/schema/relations.ts
@@ -2,6 +2,8 @@ import { relations } from 'drizzle-orm'
 
 import { cliente } from './cliente.schema'
 import { cuota } from './cuota.schema'
+import { pagoFlow } from './pago-flow.schema'
+import { pago } from './pago.schema'
 import { prestamo } from './prestamo.schema'
 
 export const clienteRelations = relations(cliente, ({ many }) => ({
@@ -20,5 +22,27 @@ export const cuotaRelations = relations(cuota, ({ one }) => ({
   prestamo: one(prestamo, {
     fields: [cuota.prestamoId],
     references: [prestamo.id]
+  }),
+  pago: one(pago, {
+    fields: [cuota.id],
+    references: [pago.cuotaId]
+  })
+}))
+
+export const pagoRelations = relations(pago, ({ one }) => ({
+  cuota: one(cuota, {
+    fields: [pago.cuotaId],
+    references: [cuota.id]
+  }),
+  pagoFlow: one(pagoFlow, {
+    fields: [pago.pagoFlowId],
+    references: [pagoFlow.id]
+  })
+}))
+
+export const pagoFlowRelations = relations(pagoFlow, ({ one }) => ({
+  pago: one(pago, {
+    fields: [pagoFlow.id],
+    references: [pago.pagoFlowId]
   })
 }))
