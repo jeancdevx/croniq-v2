a71b89290aeca87b771b5a30492a441f335c4395
Eddu <azabacheeduardo85@gmail.com>
2025-12-08 20:01:25 -0500

    feat: export delete draft action

--- modules/loans/server/index.ts
+++ modules/loans/server/index.ts
@@ -1,3 +1,4 @@
 export { activateLoan } from './activate-loan'
 export { createLoan } from './create-loan'
+export { deleteDraftLoan } from './delete-draft-loan'
 export { updateLoan } from './update-loan'
